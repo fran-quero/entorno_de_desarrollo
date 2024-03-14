a4bc8d9b52bb08488eabb87441844cad51735915
fran-quero <fjavier.fj41@gmail.com>
2024-03-14 16:09:58 +0100

    Proyecto personas creado

---
+++ CreacionAutomatica/src/models/Persona.java
@@ -0,0 +1,107 @@
+package models;
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    private String dni;
+
+    /**
+     * 
+     */
+    private String nombre;
+
+    /**
+     * 
+     */
+    private String apellido;
+
+    /**
+     * 
+     */
+    private int edad;
+
+    /**
+     * @param dni 
+     * @param nombre 
+     * @param apellido 
+     * @param edad
+     */
+    public Persona(String dni, String nombre, String apellido, int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * @param dni
+     */
+    public void setDni(String dni) {
+        // TODO implement here
+    }
+
+    /**
+     * Devuelve el dni de la persona
+     * @return
+     */
+    public String getDni() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @param nombre
+     */
+    public void setNombre(String nombre) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String getNombre() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @param apellido
+     */
+    public void setApellido(String apellido) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String getApellido() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @param edad
+     */
+    public void setEdad(int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int getEdad() {
+        // TODO implement here
+        return 0;
+    }
+
+}
